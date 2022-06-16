c4c8d852965a44bf8468e2995ad54d79aad3e009
Sami Musallam <samusall@cisco.com>
2022-06-16 12:06:11 +0300

    feat(nodejs-layer): added default propagators

--- nodejs/src/wrapper.ts
+++ nodejs/src/wrapper.ts
@@ -1,5 +1,5 @@
-import {diag, DiagConsoleLogger, DiagLogLevel} from '@opentelemetry/api';
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import {diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import {BatchSpanProcessor, SDKRegistrationConfig} from '@opentelemetry/sdk-trace-base';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
@@ -7,6 +7,10 @@ import { awsLambdaDetector } from '@opentelemetry/resource-detector-aws';
 import { detectResources, envDetector, processDetector } from '@opentelemetry/resources';
 import { Consts } from 'cisco-opentelemetry-specifications';
 import {getEnvBoolean, verifyToken} from './utils';
+import {CompositePropagator, W3CTraceContextPropagator} from "@opentelemetry/core";
+import {AWSXRayPropagator} from "@opentelemetry/propagator-aws-xray";
+import {B3InjectEncoding, B3Propagator} from "@opentelemetry/propagator-b3";
+import {AWSXRayIdGenerator} from "@opentelemetry/id-generator-aws-xray";
 
 const debug =
     getEnvBoolean(Consts.CISCO_DEBUG_ENV, Consts.DEFAULT_CISCO_DEBUG);
@@ -15,29 +19,19 @@ if(debug) {
     diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 }
 
-const ciscoToken = process.env[Consts.CISCO_TOKEN_ENV] || '';
-
-if (!ciscoToken) {
-    diag.error('Cisco token was not set');
-}
-
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 
-const instrumentations: any[] =
-    getNodeAutoInstrumentations({
-        '@opentelemetry/instrumentation-aws-lambda': {
-            disableAwsContextPropagation: true
-        },
-    })
+const instrumentations: any[] = getNodeAutoInstrumentations()
 
 async function init() {
-    const detectedResources =
+    const resource =
         await detectResources({
             detectors: [awsLambdaDetector, envDetector, processDetector]
         });
 
     const provider = new NodeTracerProvider({
-        resource: detectedResources
+        resource,
+        idGenerator: new AWSXRayIdGenerator()
     });
 
     const collectorOptions = {
@@ -51,11 +45,34 @@ async function init() {
 
     provider.addSpanProcessor(spanProcessor);
 
-    provider.register();
+    let sdkRegistrationConfig: SDKRegistrationConfig = {};
+
+    if (!process.env.OTEL_PROPAGATORS) {
+        sdkRegistrationConfig =
+            {
+                propagator:  new CompositePropagator({
+                    propagators: [
+                        new AWSXRayPropagator(),
+                        new W3CTraceContextPropagator(),
+                        new B3Propagator(),
+                        new B3Propagator({ injectEncoding: B3InjectEncoding.MULTI_HEADER }),
+                    ],
+                }),
+            };
+    }
+
+    provider.register(sdkRegistrationConfig);
 
     registerInstrumentations({
         instrumentations: instrumentations
     });
 }
 
-init();
+const ciscoToken = process.env[Consts.CISCO_TOKEN_ENV] || '';
+
+if (ciscoToken) {
+    init();
+} else {
+    diag.error('Cisco token was not found in the environment variables');
+}
+
